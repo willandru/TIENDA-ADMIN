14f8feadd24e5c661a9329f56b45c45f53a95ae8
willandru <willandru@hotmail.com>
2024-01-08 20:57:58 -0500

    TIENDA-ADMIN :: E-COMMERCE/myapp

---
+++ components/modals/store-modal.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { useStoreModal } from "@/hooks/use-strore-modal";
+import { Modal } from "@/components/ui/modal";
+import { Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { log } from "console";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+
+
+
+const formSchema = z.object({
+    name: z.string().min(3),
+});
+
+
+export const StoreModal = () => {
+    const storeModal = useStoreModal();
+
+    const [loading, setLoading] = useState(false);
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+        },
+    });
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            setLoading(true);
+
+            
+
+            const response = await axios.post('/api/stores', values);
+            console.log(response.data);
+            window.location.assign(`/${response.data.id}`);
+            
+        } catch (error) {
+            toast.error("Something went wrong");
+        } finally{
+            setLoading(false);
+        }
+    }
+
+    return (
+        <Modal
+            title="Create a store"
+            description="Add a new store to manage products and categories"
+            isOpen={storeModal.isOpen}
+            onClose={storeModal.onClose}
+        >
+            <div>
+                <div className="space-y-4 py-2 pb-4">
+
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)}>
+                            <FormField
+                                control={form.control}
+                                name="name"
+                                render={({ field }) => (
+
+                                    <FormItem>
+                                        <FormLabel>Name</FormLabel>
+                                        <FormControl>
+                                            <Input disabled={loading}
+                                             placeholder="E-commerce"
+                                            {...field} />
+                                        </FormControl>
+                                        <FormMessage/>
+                                    </FormItem>
+                                )}
+                            />
+
+                            <div className="pt-6 space-x-2 flex items-center justify-end">
+                                <Button
+                                    disabled={loading}
+                                    variant="outline"
+                                    onClick={storeModal.onClose}>
+                                        Cancel
+                                </Button>
+                                <Button 
+                                    disabled={loading}
+                                    type="submit">
+                                        Continue
+                                </Button>
+                            </div>
+
+                        </form>
+
+                    </Form>
+
+
+                </div>
+            </div>
+
+        </Modal>
+    )
+}
---
+++ middleware.ts
@@ -0,0 +1,14 @@
+import { authMiddleware } from "@clerk/nextjs";
+ 
+
+export default authMiddleware({
+  publicRoutes: ["/api/:path*"],
+});
+ 
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
+
+
+
+console.log("Middleware configured");
